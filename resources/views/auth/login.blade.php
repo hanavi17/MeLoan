ce2872f0bef1fdefe37acd89b33481413f99b5d9
hanavi17 <hanavi_alvarel_26rpl@student.smktelkom-mlg.sch.id>
2019-12-01 01:51:25 +0700

    nambah dikit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,83 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>MeLoan Login</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MeLoan login</title>
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="stylesheet" href="{{asset('vendors/iconfonts/mdi/css/materialdesignicons.min.css')}}">
-  <link rel="stylesheet" href="{{asset('vendors/iconfonts/puse-icons-feather/feather.css')}}">
-  <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.base.css')}}">
-  <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.addons.css')}}">
-  <link rel="stylesheet" href="{{asset('css/style.css')}}">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="{{asset('favicon.ico')}}" />
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/logan.css')}}">
+    <link rel="stylesheet" href="{{asset('css/res.css')}}">
+
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" />
+    
 </head>
-
 <body>
-<form method="POST" action="{{ route('login') }}">
-{{ csrf_field() }}
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper auth-page">
-      <div class="content-wrapper d-flex align-items-center auth theme-one">
-
-        <div class="row w-100">
-        <div class="col-md-12" style="margin-bottom: 20px;">
-        <h2 style="text-align: center;">MeLoan</h2>
-        </div>
-        <div class="col-lg-4 mx-auto">
-            <div class="auto-form-wrapper">
-
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}"">
-                  <label class="label">Username</label>
-                  <div class="input-group">
-                    <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-                    <div class="input-group-append">
-                      <span class="input-group-text">
-                        <i class="mdi mdi-check-circle-outline"></i>
-                      </span>
-                    </div>
-                  </div>
-                    @if ($errors->has('email'))
+    <div class="wrapper">
+        <div class="header">
+             <span>Peminjaman alat kesehatan no.1</span>
+          </div>
+      <div class="form"> 
+        <form method="POST" action="{{ route('login') }}" >
+            {{ csrf_field() }}
+            <div class="head">
+                <!-- <img src="materials/favicon.ico" alt=""> -->
+            <span class="jub">MeLoan</span>
+            </div>
+            <div class="formin">
+                <span>Hai!</span>
+                <p class="pe">Selamat datang kembali, silahkan login</p>
+              <div class="input">
+                  <input type="text" name="email" id="email"  class="" placeholder="username" value="{{ old('email') }}" required>
+                  @if ($errors->has('email'))
                         <span class="help-block">
                             <strong>{{ $errors->first('email') }}</strong>
                         </span>
                     @endif
-                </div>
-                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                  <label class="label">Password</label>
-                  <div class="input-group">
-                    <input id="password" type="password" class="form-control" name="password" required>
-                    <div class="input-group-append">
-                      <span class="input-group-text">
-                        <i class="mdi mdi-check-circle-outline"></i>
-                      </span>
-                    </div>
-                    @if ($errors->has('password'))
+                  <input type="password" name="password" id="password"  class="" placeholder="password" required>
+                  @if ($errors->has('password'))
                         <span class="help-block">
                             <strong>{{ $errors->first('password') }}</strong>
                         </span>
                     @endif
-                  </div>
-                </div>
-                <div class="form-group">
-                  <button class="btn btn-primary submit-btn btn-block" type="submit">Login</button>
-                </div>
+              </div>           
+              
+              <div class="submit">
+                  <input type="submit" name="" value="Login" class="login">
+                  
+              </div>
             </div>
-            <p class="footer-text text-center" style="margin-top: 20px;color: #308ee0">Copyright © {{date('Y')}} - All rights reserved.</p>
-          </div>
-        </div>
+        </form>
       </div>
-      <!-- content-wrapper ends Herziwp@gmail.com -->
-    </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-  </form>
-  <script src="{{asset('vendors/js/vendor.bundle.base.js')}}"></script>
-  <script src="{{asset('vendors/js/vendor.bundle.addons.js')}}"></script>
-</body>
+     </div>
 
+
+     <script src="js/jquery.js"></script>
+</body>
 </html>
